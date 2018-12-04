95cf0f73b64e5f8518dd9004f15f7bee1a337c90
sapics <gv.nishino@gmail.com>
2018-12-04 17:15:04 +0900

    Replace gutil.log to fancy-log

--- bin/gulp.js
+++ bin/gulp.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 'use strict';
-var gutil = require('gulp-util');
+var log = require('fancy-log');
 var prettyTime = require('pretty-hrtime');
 var chalk = require('chalk');
 var semver = require('semver');
@@ -46,22 +46,22 @@ var simpleTasksFlag = argv['tasks-simple'];
 var shouldLog = !argv.silent && !simpleTasksFlag;
 
 if (!shouldLog) {
-  gutil.log = function() {};
+  log = function() {};
 }
 
 cli.on('require', function(name) {
-  gutil.log('Requiring external module', chalk.magenta(name));
+  log('Requiring external module', chalk.magenta(name));
 });
 
 cli.on('requireFail', function(name) {
-  gutil.log(chalk.red('Failed to load external module'), chalk.magenta(name));
+  log(chalk.red('Failed to load external module'), chalk.magenta(name));
 });
 
 cli.on('respawn', function(flags, child) {
   var nodeFlags = chalk.magenta(flags.join(', '));
   var pid = chalk.magenta(child.pid);
-  gutil.log('Node flags detected:', nodeFlags);
-  gutil.log('Respawned to PID:', pid);
+  log('Node flags detected:', nodeFlags);
+  log('Respawned to PID:', pid);
 });
 
 cli.launch({
@@ -74,39 +74,39 @@ cli.launch({
 // The actual logic
 function handleArguments(env) {
   if (versionFlag && tasks.length === 0) {
-    gutil.log('CLI version', cliPackage.version);
+    log('CLI version', cliPackage.version);
     if (env.modulePackage && typeof env.modulePackage.version !== 'undefined') {
-      gutil.log('Local version', env.modulePackage.version);
+      log('Local version', env.modulePackage.version);
     }
     process.exit(0);
   }
 
   if (!env.modulePath) {
-    gutil.log(
+    log(
       chalk.red('Local gulp not found in'),
       chalk.magenta(tildify(env.cwd))
     );
-    gutil.log(chalk.red('Try running: npm install gulp'));
+    log(chalk.red('Try running: npm install gulp'));
     process.exit(1);
   }
 
   if (!env.configPath) {
-    gutil.log(chalk.red('No gulpfile found'));
+    log(chalk.red('No gulpfile found'));
     process.exit(1);
   }
 
   // Check for semver difference between cli and local installation
   if (semver.gt(cliPackage.version, env.modulePackage.version)) {
-    gutil.log(chalk.red('Warning: gulp version mismatch:'));
-    gutil.log(chalk.red('Global gulp is', cliPackage.version));
-    gutil.log(chalk.red('Local gulp is', env.modulePackage.version));
+    log(chalk.red('Warning: gulp version mismatch:'));
+    log(chalk.red('Global gulp is', cliPackage.version));
+    log(chalk.red('Local gulp is', env.modulePackage.version));
   }
 
   // Chdir before requiring gulpfile to make sure
   // we let them chdir as needed
   if (process.cwd() !== env.cwd) {
     process.chdir(env.cwd);
-    gutil.log(
+    log(
       'Working directory changed to',
       chalk.magenta(tildify(env.cwd))
     );
@@ -114,7 +114,7 @@ function handleArguments(env) {
 
   // This is what actually loads up the gulpfile
   require(env.configPath);
-  gutil.log('Using gulpfile', chalk.magenta(tildify(env.configPath)));
+  log('Using gulpfile', chalk.magenta(tildify(env.configPath)));
 
   var gulpInst = require(env.modulePath);
   logEvents(gulpInst);
@@ -139,7 +139,7 @@ function logTasks(env, localGulp) {
       if (v.trim().length === 0) {
         return;
       }
-      gutil.log(v);
+      log(v);
     });
 }
 
@@ -180,12 +180,12 @@ function logEvents(gulpInst) {
   gulpInst.on('task_start', function(e) {
     // TODO: batch these
     // so when 5 tasks start at once it only logs one time with all 5
-    gutil.log('Starting', '\'' + chalk.cyan(e.task) + '\'...');
+    log('Starting', '\'' + chalk.cyan(e.task) + '\'...');
   });
 
   gulpInst.on('task_stop', function(e) {
     var time = prettyTime(e.hrDuration);
-    gutil.log(
+    log(
       'Finished', '\'' + chalk.cyan(e.task) + '\'',
       'after', chalk.magenta(time)
     );
@@ -194,19 +194,19 @@ function logEvents(gulpInst) {
   gulpInst.on('task_err', function(e) {
     var msg = formatError(e);
     var time = prettyTime(e.hrDuration);
-    gutil.log(
+    log(
       '\'' + chalk.cyan(e.task) + '\'',
       chalk.red('errored after'),
       chalk.magenta(time)
     );
-    gutil.log(msg);
+    log(msg);
   });
 
   gulpInst.on('task_not_found', function(err) {
-    gutil.log(
+    log(
       chalk.red('Task \'' + err.task + '\' is not in your gulpfile')
     );
-    gutil.log('Please check the documentation for proper gulpfile formatting');
+    log('Please check the documentation for proper gulpfile formatting');
     process.exit(1);
   });
 }
